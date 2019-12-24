8076fe73ff13530c726e57a9b1511ed08e951ae4
tkddls3319 <54829486+tkddls3319@users.noreply.github.com>
2019-12-24 15:59:55 +0900

    초기 커밋.

---
+++ WinGitTest/Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinGitTest
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            this.button1.Text = "버튼클릭";
+            this.button1.Text = "버튼클릭2";
+            this.button1.Text = "버튼클릭2";
+        }
+
+        private void Button2_Click(object sender, EventArgs e)
+        {
+            this.Text = "버튼클릭";
+        }
+    }
+}
