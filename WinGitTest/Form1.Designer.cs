b8fe0b09e5d9f24467f00d8891910eea04c348b6
tkddls3319 <54829486+tkddls3319@users.noreply.github.com>
2019-12-24 16:01:44 +0900

    hgfhfgh

--- WinGitTest/Form1.Designer.cs
+++ WinGitTest/Form1.Designer.cs
@@ -47,7 +47,7 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(162, 184);
+            this.button2.Location = new System.Drawing.Point(290, 101);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(75, 23);
             this.button2.TabIndex = 1;
